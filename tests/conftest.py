c6dc2678ea0df84bafb9e58156232382079690f4
Jondi-Ts <jondi.ts@gmail.com>
2025-03-16 16:27:48 +0200

    better handeling of test_client_resource

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,7 +3,8 @@ import pytest
 from fastapi.testclient import TestClient
 from app.main import app
 
-@pytest.fixture(scope="module")
-def test_client():
-    """Provides a TestClient instance for API testing."""
-    return TestClient(app)
+
+@pytest.fixture(scope="class")
+def client():
+    with TestClient(app) as test_client:
+        yield test_client
